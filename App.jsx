ade80ba585785f6ec444b419c73e8000de1478a5
tomoaki <39047080+tomoaki1031@users.noreply.github.com>
2021-09-19 17:27:03 +0900

    reate MemoDetail screen

--- App.jsx
+++ App.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-import AppBar from './src/components/AppBar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/CircleButton';
+import MemoDetailScreen from './src/screens/MemoDetailsScreen';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-
-    </View>
+    <MemoDetailScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-
-  },
-
-});
---
+++ src/screens/MemoDetailsScreen.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+
+import Appbar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoDetailScreen() {
+  return (
+    <View style={styles.contsiner}>
+      <Appbar />
+      <View style={styles.memoHeader}>
+        <Text style={styles.memoTitle}>買い物リスト</Text>
+        <Text style={styles.memoDate}>2021年9月18日 10:00</Text>
+      </View>
+
+      <ScrollView style={styles.memoBody}>
+        <Text style={styles.memoText}>
+          lllllllllllllltrewtrewtrewtrewrtewetrewrtewtrewtewtrew
+          gfdsgdsgfdsfgfds
+          gfdsgdsgfdsfgfdsg
+          gfdsgdsgfdsfgfds
+          gfdsgdsgfdsfgfds
+
+        </Text>
+      </ScrollView>
+      <CircleButton style={{ top: 160, botom: 'auto' }}>+</CircleButton>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  memoHeader: {
+    backgroundColor: '#467FD3',
+    height: 96,
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 19,
+  },
+
+  memoTitle: {
+    color: '#ffffff',
+    fontSize: 20,
+    lineHeight: 32,
+    fontWeight: 'bold',
+  },
+
+  memoDate: {
+    color: '#ffffff',
+    fontSize: 12,
+    lineHeight: 16,
+  },
+
+  memoBody: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+
+  memoText: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+
+});
