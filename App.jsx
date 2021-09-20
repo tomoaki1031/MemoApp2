77bace085236280518a2ccc73daffdc178cbb97a
tomoaki <39047080+tomoaki1031@users.noreply.github.com>
2021-09-20 12:26:56 +0900

    create edit,create

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import MemoDetailScreen from './src/screens/MemoDetailsScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailsScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <MemoDetailScreen />
+    <MemoCreateScreen />
   );
 }
