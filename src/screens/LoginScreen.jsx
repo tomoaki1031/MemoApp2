9fa669680e04916b9a7612b0e7788170b11ffae4
tomoaki <39047080+tomoaki1031@users.noreply.github.com>
2021-09-27 10:45:59 +0900

    create Logoutbutton

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View, Text, TextInput, StyleSheet,
   TouchableOpacity,
@@ -12,6 +12,18 @@ export default function loginScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'MemoList' }],
+        });
+      }
+    });
+    return unsubscribe;
+  }, []);
 
   function handlePress() {
     firebase.auth().signInWithEmailAndPassword(email, password)
